e5b98768a1de0ba58332e13d87e4e5759ee3347f
Vadym <68814002+VadymTori@users.noreply.github.com>
2023-03-25 14:05:31 -0400

    add Hello word
    this is condition for homework

--- javascript.js
+++ javascript.js
@@ -8,4 +8,5 @@ let c=Number(prompt('Input result of (-40+79)'));
 //Now lets check if condition and outup results with correct text
 if ((a)===10 && b === 40 && c===39) document.write('Hello, human! You vault key was: '+(a)+' - '+(b)+' - '+(c)+' and the vault is open!');
 else (document.write('You vault code '+(a)+' - '+(b)+' - '+(c)+' is wrong, DIE HUMAN! <br /><b>Press refresh to try again!</b>'));
+document.write('Hello world!')'
 //Finish :)
