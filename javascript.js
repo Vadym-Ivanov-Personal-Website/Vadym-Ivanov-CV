64f08d6201bfcd733b5981add3db04360c7c5e85
Vadym <68814002+VadymTori@users.noreply.github.com>
2023-03-25 13:54:01 -0400

    Unit7
    Homework from unit 7

---
+++ javascript.js
@@ -0,0 +1,11 @@
+//Correct answher:
+//a = 10;
+//b = 40;
+//c = 39;
+let a=Number(prompt('Input result of 10%20'));
+let b=Number(prompt('Input result of 100-30*2')); 
+let c=Number(prompt('Input result of (-40+79)'));
+//Now lets check if condition and outup results with correct text
+if ((a)===10 && b === 40 && c===39) document.write('Hello, human! You vault key was: '+(a)+' - '+(b)+' - '+(c)+' and the vault is open!');
+else (document.write('You vault code '+(a)+' - '+(b)+' - '+(c)+' is wrong, DIE HUMAN! <br /><b>Press refresh to try again!</b>'));
+//Finish :)
